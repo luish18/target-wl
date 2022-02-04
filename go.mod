8ad96f71c86fb4f092c6f1e8a7e83cf266e3cae6
Luís Henrique de Almeida Fernandes <luish.fernandes@usp.br>
2022-02-04 16:28:14 -0300

    adds database integration

--- go.mod
+++ go.mod
@@ -4,7 +4,11 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/marco-developer/dasvid/poclib v1.0.0
+	github.com/mattn/go-sqlite3 v1.14.11 // indirect
 	github.com/spiffe/go-spiffe/v2 v2.0.0-beta.11 // indirect
+	google.golang.org/grpc v1.33.2 // indirect
+	gopkg.in/square/go-jose.v2 v2.4.1 // indirect
+	modernc.org/sqlite v1.14.6 // indirect
 )
 
 replace github.com/marco-developer/dasvid/poclib v1.0.0 => ./poclib
--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
+    "context"
+    "encoding/json"
+    "errors"
+    "fmt"
+    "io/ioutil"
+    "log"
     "net"
-	"strings"
+    "net/http"
+    "strings"
 
-	// SPIFFE
-	"github.com/spiffe/go-spiffe/v2/spiffeid"
-	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
-	"github.com/spiffe/go-spiffe/v2/svid/x509svid"
-	"github.com/spiffe/go-spiffe/v2/workloadapi"
+    //Database
+    "database/sql"
 
-	// dasvid lib
-	dasvid "github.com/marco-developer/dasvid/poclib"
+    // SPIFFE
+    "github.com/spiffe/go-spiffe/v2/spiffeid"
+    "github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
+    "github.com/spiffe/go-spiffe/v2/workloadapi"
+
+    // dasvid lib
+    dasvid "github.com/marco-developer/dasvid/poclib"
 )
 
 const (
@@ -40,21 +43,45 @@ type PocData struct {
 
 
 type Balance struct {
+    Account_id  string `json:",omitempty"`
     Balance     string `json:",omitempty"`
 }
 
 func main(){
 
+
+
     // creates empty context to recieve an incoming request
     ctx, cancel := context.WithCancel(context.Background())
     defer cancel()
+
+    http.HandleFunc("/get_balance", get_data)
+    http.HandleFunc("/update_balance", update_data)
+
 
     source, err := workloadapi.NewX509Source(ctx, workloadapi.WithClientOptions(workloadapi.WithAddr(socketPath)))
     if err != nil {
         log.Fatalf("Unable to create X509 source: %v", err)
     }
     defer source.Close()
+
+    // Allowed SPIFFE ID - Client must be from this trust domain
+    clientID := spiffeid.RequireTrustDomainFromString("example.org")
+
+    // Create a `tls.Config` to allow mTLS connections, and verify that presented certificate match the allowed SPIFFE-ID
+    tlsConfig := tlsconfig.MTLSServerConfig(source, source, tlsconfig.AuthorizeMemberOf(clientID))
+    server := &http.Server{
+        Addr:      ":8443",
+        TLSConfig: tlsConfig,
+    }
+
+    log.Printf("Start serving API...")
+    if err := server.ListenAndServeTLS("", ""); err != nil {
+        log.Fatalf("Error on serve: %v", err)
+    }
+
 }
+
 
 
 func GetOutboundIP() net.IP {
@@ -69,7 +96,7 @@ func GetOutboundIP() net.IP {
     return localAddr.IP
 }
 
-func validate_dasvid(data string) bool {
+func validate_dasvid(data string) (bool, error) {
 
     ctx, cancel := context.WithCancel(context.Background())
     defer cancel()
@@ -119,22 +146,31 @@ func validate_dasvid(data string) bool {
     var result PocData
     json.Unmarshal(body, &result)
 
-    if (*result.DasvidExpValidation) && (*result.DasvidSigValidation) {
-        return true
+
+    //validating response
+    if !(*result.DasvidExpValidation){
+
+        return false, errors.New("DASVID expired!")
     }
 
-    return false
+    if !(*result.DasvidSigValidation){
 
+        return false, errors.New("Invalid DASVID signature")
+    }
+
+    return true, nil
 }
 
 
 func get_data(w http.ResponseWriter, r *http.Request){
 
     data := r.FormValue("DASVID")
-    if !(validate_dasvid(data)) {
+    validate_result, err := validate_dasvid(data)
 
-        log.Printf("Invalid DA-SVID")
-        json.NewEncoder(w).Encode(nil)
+    if !(validate_result) {
+
+        log.Fatalf("Invalid DA-SVID: %v", err)
+        json.NewEncoder(w).Encode(err)
         return
     }
 
@@ -144,17 +180,94 @@ func get_data(w http.ResponseWriter, r *http.Request){
     if dasvid_claims["sub"].(string) != "web"{
 
         log.Printf("Unauthorized subject workload!")
-        json.NewEncoder(w).Encode(nil)
+        json.NewEncoder(w).Encode("Unauthorized subject workload")
         return
     }
 
-    //TODO:make data request
 
-    responde
-    json.NewEncoder(w).Encode()
+    var db *sql.DB
+
+    db, err = sql.Open("sqlite3", "./balances.db")
+    if err != nil {
+
+        log.Fatalf("Unable to open database balances.db: %v", err)
+    }
+    defer db.Close()
+
+    var response Balance
+    account_id := r.FormValue("account_id")
+
+    //query database for account id
+    query := "select" + account_id + "from balances"
+    rows, err := db.Query(query)
+    if err != nil {
+
+        log.Fatalf("Unable to query database")
+    }
+    defer rows.Close()
 
 
-    //TODO:write data on response writer
+    for rows.Next() {
 
+        err = rows.Scan(&response.Account_id, &response.Balance)
+        if err != nil {
+
+            log.Fatalf("Unable to read rows")
+        }
+    }
+    log.Println("Read %v with balance %v from database", response.Account_id, response.Balance)
+
+
+    json.NewEncoder(w).Encode(response)
+
+}
+
+
+func update_data(w http.ResponseWriter, r *http.Request){
+
+    data := r.FormValue("DASVID")
+    validate_result, err := validate_dasvid(data)
+
+    if !(validate_result) {
+
+        log.Fatalf("Invalid DA-SVID: %v", err)
+        json.NewEncoder(w).Encode(err)
+        return
+    }
+
+
+    //in this example we will consider that only "web" subjects will be able request data
+    dasvid_claims := dasvid.ParseTokenClaims(data)
+    if dasvid_claims["sub"].(string) != "web"{
+
+        log.Printf("Unauthorized subject workload!")
+        json.NewEncoder(w).Encode("Unauthorized subject workload")
+        return
+    }
+
+
+    var db *sql.DB
+
+    db, err = sql.Open("sqlite3", "./balances.db")
+    if err != nil {
+
+        log.Fatalf("Unable to open database balances.db: %v", err)
+    }
+    defer db.Close()
+
+    var response Balance
+    account_id := r.FormValue("account_id")
+    new_balance := r.FormValue("balance")
+
+    //query database for account id
+    query := "update balances SET balance=" + new_balance + "where account_id=" + account_id
+    _, err = db.Exec(query)
+    if err != nil {
+
+        log.Fatalf("Unable to update database")
+    }
+
+
+    json.NewEncoder(w).Encode(response)
 
 }
